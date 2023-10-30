59ee5ec964956fc33bc632d04375a459bc4e2d64
vengottip <110312357+vengottip@users.noreply.github.com>
2023-10-29 22:41:28 -0400

    ssl verfications are disabled

--- post_api_client/post_rest_api2.py
+++ post_api_client/post_rest_api2.py
@@ -1,4 +1,8 @@
 import requests
+import urllib3
+
+# Disable SSL certificate verification
+urllib3.disable_warnings()
 
 # API endpoint URL
 api_url = "https://example.com/api/endpoint"
@@ -6,26 +10,14 @@ api_url = "https://example.com/api/endpoint"
 # Authorization code
 authorization_code = "xxxxxxx"
 
-# Path to the Word document
-document_path = "c:/abc.docx"
-
-# Data to be sent as binary
-binary_data = b"Your binary data here"
-
 # Create the headers with the authorization token
 headers = {
     "Authorization": f"Bearer {authorization_code}",
 }
 
-# Create a dictionary for the file to be sent
-files = {
-    "file": ("abc.docx", open(document_path, "rb")),
-    "data-binary": ("data.bin", binary_data)
-}
-
-# Send the POST request
+# Send the POST request with SSL certificate verification disabled
 try:
-    response = requests.post(api_url, headers=headers, files=files)
+    response = requests.post(api_url, headers=headers, verify=False)
 
     # Check if the request was successful (status code 200)
     if response.status_code == 200:
