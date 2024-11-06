5d915db50dc833df5003e841600158cc6ec0ad1b
vengottip <110312357+vengottip@users.noreply.github.com>
2024-11-05 19:26:59 -0500

    Update oracle_query.py

--- oracle_queries/oracle_query.py
+++ oracle_queries/oracle_query.py
@@ -31,13 +31,14 @@ metadata_df = pd.read_csv('path_to_your_csv_file.csv')
 # Set up Oracle connection (replace with your actual credentials)
 connection = cx_Oracle.connect(user="your_username", password="your_password", dsn="your_dsn")
 cursor = connection.cursor()
-
+metadata_df['PK_COL_VAL'] = metadata_df['PK_COL_VAL'].apply(lambda x: int(float(x)) if pd.notnull(x) else x)
 # Loop through each row in the CSV
 for index, row in metadata_df.iterrows():
     table_name = row['TBL_NM']
     column_name = row['COL_NM']
     pk_column = row['PK_COL']
     pk_value = row['PK_COL_VAL']
+    
 
     # Construct the query to get data from tables in schema B
     query = f"SELECT * FROM B.{table_name} WHERE {pk_column} = :pk_value"
